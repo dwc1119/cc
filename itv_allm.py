cf735de3f1d846f6d46911851076932b8c9efc73
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-11 12:24:35 +0800

    Update itv_allm.py

--- itv_allm.py
+++ itv_allm.py
@@ -244,7 +244,7 @@ def worker():
                         results.append(result)
                         numberx = (len(results) + len(error_channels)) / len(channels) * 100
                         print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
-                        break
+                        #break
         except Exception:
             # 无法连接并超时的情况下输出“X”
             error_channel = channel_name, channel_url
