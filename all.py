d059a8538bd4cad8463bbd3acbb5604443a3b3e5
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-13 08:41:07 +0800

    Update all.py

--- all.py
+++ all.py
@@ -200,7 +200,7 @@ with open("itvlist.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('央视频道,#genre#\n')
     for result in results:
-        channel_name, channel_url, speed = result
+        channel_name, channel_url
         if 'CCTV' in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
