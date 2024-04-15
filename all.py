2719ccbd6271fc83454513e0117096472e78f079
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-15 08:22:44 +0800

    Update all.py

--- all.py
+++ all.py
@@ -203,9 +203,7 @@ for url in urls:
                 continue
         except:
             continue
-def save_results(results, itv):
-    # 将结果保存到文本文件
-    with open(itv, "w", encoding="utf-8") as file:
-        for result in results:
-            file.write(result + "\n")
-            print(result)
+with open(itv.txt, "w", encoding="utf-8") as file:
+    for result in results:
+        file.write(result + "\n")
+        print(result)
