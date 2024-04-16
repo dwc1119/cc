ab508f01e8b46f33ec8a02f62af0e4da6a6b2906
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-16 13:42:54 +0800

    Update all.py

--- all.py
+++ all.py
@@ -5,6 +5,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 import re
+import threading
+from queue import Queue
+import eventlet
+eventlet.monkey_patch()
+
 ###urls城市根据自己所处地理位置修改
 urls = [
     "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iY2hhbmdzaGEi",  # changsha 长沙
