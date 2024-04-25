bf8802e5e45c6fb58e082d8ef2f865da415a769b
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-25 16:12:58 +0800

    Update a.py

--- a.py
+++ a.py
@@ -26,14 +26,14 @@ def modify_urls(url):
     base_url = url[:ip_start_index]  # http:// or https://
     ip_address = url[ip_start_index:ip_end_index]
     port = url[ip_end_index:]
-    ip_end = "/status"
+    ip_end = "/rtp/239.254.201.152:7205"
     for i in range(1, 256):
         modified_ip = f"{ip_address[:-1]}{i}"
         modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
         modified_urls.append(modified_url)
 
     return modified_urls
-    print(url)
+    
 
 
 def is_url_accessible(url):
@@ -41,7 +41,6 @@ def is_url_accessible(url):
         response = requests.get(url, timeout=1)
         if response.status_code == 200:
             return url
-            print(url)
     except requests.exceptions.RequestException:
         pass
     return None
