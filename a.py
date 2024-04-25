e4704b1efa560f592dc1fbd8b61d72172d4a4acc
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-25 13:22:02 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,19 +1,91 @@
-import socket
+import time
+import os
+import concurrent.futures
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+import re
+import threading
+from queue import Queue
+import eventlet
+eventlet.monkey_patch()
 
-# 创建socket对象
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+###urls城市根据自己所处的地理位置修改
+urls = [
+    #"https://fofa.info/result?qbase64=572R57ucVFbnrqHnkIbns7vnu58%3D",
+    #"http://tonkiang.us/",
+    #"https://fofa.info/result?qbase64=IuS6v%2BmUi%2BeOi%2BWdpCI%3D",
+    "https://fofa.info/result?qbase64=IuWFrOaYjuaWsOWbtOenkea6kOe9kee7nCI%3D",#深圳
+    #"https://fofa.info/result?qbase64=Iue9kee7nFRW566h55CG57O757ufIg%3D%3D"
+]
 
-# 连接服务器
-server_address = ('111.227.237.82', 4022)
-client_socket.connect(server_address)
+def modify_urls(url):
+    modified_urls = []
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_end_index]
+    port = url[ip_end_index:]
+    ip_end = "/ZHGXTV/Public/json/live_interface.txt"
+    for i in range(1, 256):
+        modified_ip = f"{ip_address[:-1]}{i}"
+        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
+        modified_urls.append(modified_url)
 
-# 发送数据
-data = 'Hello, Server!'
-client_socket.sendall(data.encode())
+    return modified_urls
 
-# 接收数据
-response = client_socket.recv(1024)
-print('接收到的数据:', response.decode())
 
-# 关闭连接
-client_socket.close()
+def is_url_accessible(url):
+    try:
+        response = requests.get(url, timeout=1)
+        if response.status_code == 200:
+            return url
+    except requests.exceptions.RequestException:
+        pass
+    return None
+
+
+results = []
+
+for url in urls:
+    try:
+        # 创建一个Chrome WebDriver实例
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+    
+        driver = webdriver.Chrome(options=chrome_options)
+        # 使用WebDriver访问网页
+        driver.get(url)  # 将网址替换为你要访问的网页地址
+        time.sleep(10)
+        # 获取网页内容
+        page_content = driver.page_source
+    
+        # 关闭WebDriver
+        driver.quit()
+    
+        # 查找所有符合指定格式的网址
+        pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+        urls_all = re.findall(pattern, page_content)
+        # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+        urls = set(urls_all)  # 去重得到唯一的URL列表
+        x_urls = []
+        for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
+            url = url.strip()
+            ip_start_index = url.find("//") + 2
+            ip_end_index = url.find(":", ip_start_index)
+            ip_dot_start = url.find(".") + 1
+            ip_dot_second = url.find(".", ip_dot_start) + 1
+            ip_dot_three = url.find(".", ip_dot_second) + 1
+            base_url = url[:ip_start_index]  # http:// or https://
+            ip_address = url[ip_start_index:ip_dot_three]
+            port = url[ip_end_index:]
+            ip_end = "1"
+            modified_ip = f"{ip_address}{ip_end}"
+            x_url = f"{base_url}{modified_ip}{port}"
+            x_urls.append(x_url)
+        urls = set(x_urls)  # 去重得到唯一的URL列表
+    
+        valid_urls = []
+        #   多线程获取可用url
