c7c82d29e7543e2168d19db4038eb0d964c6bf32
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-26 17:39:31 +0800

    Update a.py

--- a.py
+++ a.py
@@ -141,19 +141,6 @@ for result in results:
         channel_name, channel_url = result.split(',')
         channels.append((channel_name, channel_url))
 
-def channel_key(channel_name):
-    match = re.search(r'\d+', channel_name)
-    if match:
-        return int(match.group())
-    else:
-        return float('inf')  # 返回一个无穷大的数字作为关键字
-
-# 对频道进行排序
-float = float.lstrip().rstrip()
-results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
-results.sort(key=lambda x: channel_key(x[0]))
-
-
 result_counter = 3  # 每个频道需要的个数
 
 with open("itvlist.txt", 'w', encoding='utf-8') as file:
