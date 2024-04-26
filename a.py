b0584d7d60ea480544266ace3f6ac8c10b8d3660
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-26 16:12:20 +0800

    Update a.py

--- a.py
+++ a.py
@@ -128,10 +128,11 @@ with open("iptv.txt", 'r', encoding='utf-8') as file:
         line = line.strip()
         if line:
             channel_name, channel_url = line.split(',')
-            channel_url = re.sub("http://111.227.237.82:4022",udpxy_url,channel_url)
-            results.append(f"{channel_name},{channel_url}")
-            for result in results:
-                 print(result)
+            for udpxy_url in udpxy_urls:
+                channel_url = re.sub("http://111.227.237.82:4022",udpxy_url,channel_url)
+                results.append(f"{channel_name},{channel_url}")
+                for result in results:
+                    print(result)
 
 
                 
