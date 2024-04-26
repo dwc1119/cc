e77b8799949acba095d7b3141483a7f051d84f31
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-26 17:10:30 +0800

    Update a.py

--- a.py
+++ a.py
@@ -149,7 +149,7 @@ def channel_key(channel_name):
         return float('inf')  # 返回一个无穷大的数字作为关键字
 
 # 对频道进行排序
-results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
+#results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 results.sort(key=lambda x: channel_key(x[0]))
 
 
