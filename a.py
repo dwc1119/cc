6bfef03f45c3c0f73f83f025d54bdfb95532f35d
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-27 18:40:07 +0800

    Update a.py

--- a.py
+++ a.py
@@ -110,13 +110,13 @@ for url in urls:
 udpxy_urls = []
 for url in valid_urls:
     # 修改文件转发地址
-    ip_start_index = url.find("//") + 2
-    ip_dot_start = url.find(".") + 1
-    ip_index_second = url.find("/", ip_dot_start)
-    base_url = url[:ip_start_index]  # http:// or https://
-    ip_address = url[ip_start_index:ip_index_second]
-    url_x = f"{base_url}{ip_address}"
-    udpxy_url = f"{url_x}"
+    #ip_start_index = url.find("//") + 2
+    #ip_dot_start = url.find(".") + 1
+    #ip_index_second = url.find("/", ip_dot_start)
+    #base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url.find(http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4})
+    #url_x = f"{ip_address}"
+    udpxy_url = f"{ip_address}"
     udpxy_urls.append(udpxy_url)
             
 for udpxy_url in udpxy_urls:
