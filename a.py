6691f2cb4c02eaf5d118b20a9150ccc3171a53c0
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-27 15:22:41 +0800

    Update a.py

--- a.py
+++ a.py
@@ -14,7 +14,7 @@ eventlet.monkey_patch()
 urls = [
     #"https://fofa.info/result?qbase64=572R57ucVFbnrqHnkIbns7vnu58%3D",
     #"http://tonkiang.us/",
-    #"https://fofa.info/result?qbase64=IuS6v%2BmUi%2BeOi%2BWdpCI%3D",
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIHJlZ2lvbj0iaGViZWki",
     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9cWluaHVhbmdkYW8%3D",#秦皇岛
     #"https://fofa.info/result?qbase64=Iue9kee7nFRW566h55CG57O757ufIg%3D%3D"
 ]
