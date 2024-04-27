26bb97bbbeca017b173a97427ba45d0adfb760e4
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-27 13:40:54 +0800

    Update a.py

--- a.py
+++ a.py
@@ -143,7 +143,7 @@ results = set(results)   # 去重得到唯一的URL列表
 results = sorted(results)
  
 for result in results:
-    line = result.strip()
+    line = result.readlines()
     if result:
         channel_name, channel_url = result.split(',')
         result = channel_name, channel_url
