cc1bea2fd973fc2045f2288361b36e13693ecb8e
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-28 12:08:47 +0800

    Update a.py

--- a.py
+++ a.py
@@ -89,7 +89,7 @@ for url in urls:
     
 mvalid_urls = []
 #   多线程获取可用url
-with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
     futures = []
     for url in urls:
         url = url.strip()
