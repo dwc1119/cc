e11750ea83562729beb5344ea48cdd8f6e624efc
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-28 19:35:32 +0800

    Update a.py

--- a.py
+++ a.py
@@ -88,19 +88,19 @@ for url in urls:
         urls = set(x_urls)  # 去重得到唯一的URL列表
     mvalid_urls = []
     #   多线程获取可用url
-with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-    futures = []
-    for url in urls:
-        url = url.strip()
-        modified_urls = modify_urls(url)
-        for modified_url in modified_urls:
-            futures.append(executor.submit(is_url_accessible, modified_url))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        futures = []
+        for url in urls:
+            url = url.strip()
+            modified_urls = modify_urls(url)
+            for modified_url in modified_urls:
+                futures.append(executor.submit(is_url_accessible, modified_url))
 
                 
-    for future in concurrent.futures.as_completed(futures):
-        result = future.result()
-        if result:
-            mvalid_urls.append(result)
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result:
+                mvalid_urls.append(result)
 
     valid_urls = []
     valid_urls = set(mvalid_urls)
