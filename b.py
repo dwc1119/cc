0d1730a7da8ccfd9b2181bc7aaca15c14e129987
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-29 20:30:01 +0800

    Update b.py

--- b.py
+++ b.py
@@ -112,20 +112,20 @@ for url in valid_urls:
     ip_address = url[ip_start_index:ip_index_second]
     url_x = f"{base_url}{ip_address}"
     udpxy_url = f"{url_x}"
-    udpxy_urls.append(udpxy_url)
+    results.append(udpxy_url)
                      
     
 
-results = []
+reads = []
 channel_udpxy_urls = []
 with open("iptv2.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
         #print(line)
-        result = line.strip()
+        reads = line.strip()
         if line:
-            channel_name,channel_url = result.split(",")
-            for udpxy_url in udpxy_urls:
+            channel_name,channel_url = read.split(",")
+            for udpxy_url in results:
                 #print(udpxy_url)
                 channel_udpxy_url = f"{udpxy_url}/{channel_url}"
                 result = f"{channel_name},{channel_udpxy_url}"
