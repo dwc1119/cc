23c034f397d3189ee9bfedc178b8d936987f9858
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-29 11:53:00 +0800

    Update a.py

--- a.py
+++ a.py
@@ -19,67 +19,90 @@ urls = [
     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIHJlZ2lvbj0iaGViZWki"#河北
 ]
 
-for url in urls:
-    print(url)
-    # 创建一个Chrome WebDriver实例
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    
-    driver = webdriver.Chrome(options=chrome_options)
-    # 使用WebDriver访问网页
-    driver.get(url)  # 将网址替换为你要访问的网页地址
-    time.sleep(10)
-    # 获取网页内容
-    page_content = driver.page_source
-    
-    # 关闭WebDriver
-    driver.quit()
 
-# 查找所有符合指定格式的网址
-pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
-urls_all = re.findall(pattern, page_content)
-# urls = list(set(urls_all))  # 去重得到唯一的URL列表
-urls = set(urls_all)  # 去重得到唯一的URL列表
-modified_urls = []
-for url in urls:
-    print(url)
+def modify_urls(url):
+    modified_urls = []
     ip_start_index = url.find("//") + 2
     ip_end_index = url.find(":", ip_start_index)
     base_url = url[:ip_start_index]  # http:// or https://
     ip_address = url[ip_start_index:ip_end_index]
     port = url[ip_end_index:]
     ip_end = "/status"
-    modified_ip = f"{ip_address}"
-    modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
-    modified_urls.append(modified_url)
-    #print(modified_url)
+    for i in range(1, 256):
+        modified_ip = f"{ip_address[:-1]}{i}"
+        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
+        modified_urls.append(modified_url)
 
-valid_urls = []        
-x_urls = []        
-for modified_url in modified_urls:
-    print(modified_url)
+    return modified_urls
+
+
+def is_url_accessible(url):
     try:
-        response = requests.get(modified_url, timeout=0.5)
+        response = requests.get(url, timeout=0.5)
         if response.status_code == 200:
-            url = modified_url.strip()
-            ip_start_index = url.find("//") + 2
-            ip_end_index = url.find(":", ip_start_index)
-            ip_dot_start = url.find(".") + 1
-            ip_dot_second = url.find(".", ip_dot_start) + 1
-            ip_dot_three = url.find(".", ip_dot_second) + 1
-            base_url = url[:ip_start_index]  # http:// or https://
-            ip_address = url[ip_start_index:ip_dot_three]
-            port = url[ip_end_index:]
-            ip_end = url[ip_dot_three:ip_end_index]
-            modified_ip = f"{ip_address}{ip_end}"
-            x_url = f"{base_url}{modified_ip}{port}"
-            x_urls.append(x_url)
-            print(x_url)
-            valid_urls = set(x_urls)  # 去重得到唯一的URL列表
-    except:
-        continue
-        
+            return url
+    except requests.exceptions.RequestException:
+        pass
+    return None
+
+
+results = []
+
+for url in urls:
+    # 创建一个Chrome WebDriver实例
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    # 使用WebDriver访问网页
+    driver.get(url)  # 将网址替换为你要访问的网页地址
+    time.sleep(10)
+    # 获取网页内容
+    page_content = driver.page_source
+
+    # 关闭WebDriver
+    driver.quit()
+
+    # 查找所有符合指定格式的网址
+    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+    urls_all = re.findall(pattern, page_content)
+    # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+    urls = set(urls_all)  # 去重得到唯一的URL列表
+    x_urls = []
+    for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
+        url = url.strip()
+        ip_start_index = url.find("//") + 2
+        ip_end_index = url.find(":", ip_start_index)
+        ip_dot_start = url.find(".") + 1
+        ip_dot_second = url.find(".", ip_dot_start) + 1
+        ip_dot_three = url.find(".", ip_dot_second) + 1
+        base_url = url[:ip_start_index]  # http:// or https://
+        ip_address = url[ip_start_index:ip_dot_three]
+        port = url[ip_end_index:]
+        ip_end = "1"
+        modified_ip = f"{ip_address}{ip_end}"
+        x_url = f"{base_url}{modified_ip}{port}"
+        x_urls.append(x_url)
+    urls = set(x_urls)  # 去重得到唯一的URL列表
+
+    valid_urls = []
+    #   多线程获取可用url
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        futures = []
+        for url in urls:
+            url = url.strip()
+            modified_urls = modify_urls(url)
+            for modified_url in modified_urls:
+                futures.append(executor.submit(is_url_accessible, modified_url))
+
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result:
+                valid_urls.append(result)
+
+    for url in valid_urls:
+        print(url)
         
 
