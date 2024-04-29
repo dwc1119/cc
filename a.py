4407cdce1a97022cd1a970debd9b40e153085c52
dwc <159391114+dwc1119@users.noreply.github.com>
2024-04-29 18:12:48 +0800

    Update a.py

--- a.py
+++ a.py
@@ -14,7 +14,7 @@ eventlet.monkey_patch()
 urls = [
     #"https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIHJlZ2lvbj0iaGViZWki",#河北
     #"https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9InRhbmdzaGFuIg%3D%3D",#唐山
-    #"https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIHJlZ2lvbj0iaGViZWki",#河北
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIHJlZ2lvbj0iaGViZWki",#河北
     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9cWluaHVhbmdkYW8%3D",#秦皇岛
     #"https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIHJlZ2lvbj0iaGViZWki"#河北
 ]
@@ -49,85 +49,88 @@ def is_url_accessible(url):
 results = []
 
 for url in urls:
-    # 创建一个Chrome WebDriver实例
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-
-    driver = webdriver.Chrome(options=chrome_options)
-    # 使用WebDriver访问网页
-    driver.get(url)  # 将网址替换为你要访问的网页地址
-    time.sleep(10)
-    # 获取网页内容
-    page_content = driver.page_source
-
-    # 关闭WebDriver
-    driver.quit()
-
-    # 查找所有符合指定格式的网址
-    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
-    urls_all = re.findall(pattern, page_content)
-    # urls = list(set(urls_all))  # 去重得到唯一的URL列表
-    urls = set(urls_all)  # 去重得到唯一的URL列表
-    x_urls = []
-    for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
-        url = url.strip()
-        ip_start_index = url.find("//") + 2
-        ip_end_index = url.find(":", ip_start_index)
-        ip_dot_start = url.find(".") + 1
-        ip_dot_second = url.find(".", ip_dot_start) + 1
-        ip_dot_three = url.find(".", ip_dot_second) + 1
-        base_url = url[:ip_start_index]  # http:// or https://
-        ip_address = url[ip_start_index:ip_dot_three]
-        port = url[ip_end_index:]
-        ip_end = "1"
-        modified_ip = f"{ip_address}{ip_end}"
-        x_url = f"{base_url}{modified_ip}{port}"
-        x_urls.append(x_url)
-    urls = set(x_urls)  # 去重得到唯一的URL列表
-
-    valid_urls = []
-    #   多线程获取可用url
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-        futures = []
-        for url in urls:
+    try:
+        # 创建一个Chrome WebDriver实例
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+    
+        driver = webdriver.Chrome(options=chrome_options)
+        # 使用WebDriver访问网页
+        driver.get(url)  # 将网址替换为你要访问的网页地址
+        time.sleep(10)
+        # 获取网页内容
+        page_content = driver.page_source
+    
+        # 关闭WebDriver
+        driver.quit()
+    
+        # 查找所有符合指定格式的网址
+        pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+        urls_all = re.findall(pattern, page_content)
+        # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+        urls = set(urls_all)  # 去重得到唯一的URL列表
+        x_urls = []
+        for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
             url = url.strip()
-            modified_urls = modify_urls(url)
-            for modified_url in modified_urls:
-                futures.append(executor.submit(is_url_accessible, modified_url))
-
-        for future in concurrent.futures.as_completed(futures):
-            result = future.result()
-            if result:
-                valid_urls.append(result)    
-    udpxy_urls = []# 修改文件转发地址
-    for url in valid_urls:
-        print(f"可用url:{url}")
-        try:
             ip_start_index = url.find("//") + 2
+            ip_end_index = url.find(":", ip_start_index)
             ip_dot_start = url.find(".") + 1
-            ip_index_second = url.find("/", ip_dot_start)
+            ip_dot_second = url.find(".", ip_dot_start) + 1
+            ip_dot_three = url.find(".", ip_dot_second) + 1
             base_url = url[:ip_start_index]  # http:// or https://
-            ip_address = url[ip_start_index:ip_index_second]
-            url_x = f"{base_url}{ip_address}"
-            udpxy_url = f"{url_x}"
-            udpxy_urls.append(udpxy_url)
+            ip_address = url[ip_start_index:ip_dot_three]
+            port = url[ip_end_index:]
+            ip_end = "1"
+            modified_ip = f"{ip_address}{ip_end}"
+            x_url = f"{base_url}{modified_ip}{port}"
+            x_urls.append(x_url)
+        urls = set(x_urls)  # 去重得到唯一的URL列表
+
+        valid_urls = []
+        #   多线程获取可用url
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            futures = []
+            for url in urls:
+                url = url.strip()
+                modified_urls = modify_urls(url)
+                for modified_url in modified_urls:
+                    futures.append(executor.submit(is_url_accessible, modified_url))
+
+            for future in concurrent.futures.as_completed(futures):
+                result = future.result()
+                if result:
+                    valid_urls.append(result)    
+        udpxy_urls = []# 修改文件转发地址
+        for url in valid_urls:
+            print(f"可用url:{url}")
             try:
-                results = []
-                channel_udpxy_urls = []
-                with open("iptv.txt", 'r', encoding='utf-8') as file:
-                    lines = file.readlines()
-                    for line in lines:
-                        #print(line)
-                        result = line.strip()
-                        if line:
-                            channel_name,channel_url = result.split(",")
-                            for udpxy_url in udpxy_urls:
-                                #print(udpxy_url)
-                                channel_udpxy_url = f"{udpxy_url}/{channel_url}"
-                                result = f"{channel_name},{channel_udpxy_url}"
-                                results.append(result)
+                ip_start_index = url.find("//") + 2
+                ip_dot_start = url.find(".") + 1
+                ip_index_second = url.find("/", ip_dot_start)
+                base_url = url[:ip_start_index]  # http:// or https://
+                ip_address = url[ip_start_index:ip_index_second]
+                url_x = f"{base_url}{ip_address}"
+                udpxy_url = f"{url_x}"
+                udpxy_urls.append(udpxy_url)
+                try:
+                    results = []
+                    channel_udpxy_urls = []
+                    with open("iptv.txt", 'r', encoding='utf-8') as file:
+                        lines = file.readlines()
+                        for line in lines:
+                            #print(line)
+                            result = line.strip()
+                            if line:
+                                channel_name,channel_url = result.split(",")
+                                for udpxy_url in udpxy_urls:
+                                    #print(udpxy_url)
+                                    channel_udpxy_url = f"{udpxy_url}/{channel_url}"
+                                    result = f"{channel_name},{channel_udpxy_url}"
+                                    results.append(result)
+                except:
+                    continue   
             except:
                 continue      
         except:
