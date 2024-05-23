5ce963bd29c743239b1c7ab9e49f63f8cee7c651
dwc <159391114+dwc1119@users.noreply.github.com>
2024-05-23 18:47:21 +0800

    Update a.py

--- a.py
+++ a.py
@@ -122,7 +122,8 @@ for url in urls:
             driver.quit()
         
             # 查找所有符合指定格式的网址
-            if "<td>1</td>" or "<td>2</td>" or "<td>3</td>" or "<td>4</td>"  or "<td>5</td>" or "<td>6</td>" or "<td>7</td>" or "<td>8</td>" in page_content:
+            #if "<td>1</td>" or "<td>2</td>" or "<td>3</td>" or "<td>4</td>"  or "<td>5</td>" or "<td>6</td>" or "<td>7</td>" or "<td>8</td>" in page_content:
+              if "<td>2</td>" in page_content:  
                 print(url) 
                 udpxy_urls = []# 修改文件转发地址
                 ip_start_index = url.find("//") + 2
