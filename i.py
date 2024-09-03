a9f42d1c80111c37ee62fe55d3d04af2da980530
dwc <159391114+dwc1119@users.noreply.github.com>
2024-09-03 18:23:21 +0800

    Update i.py

--- i.py
+++ i.py
@@ -11,7 +11,7 @@ import eventlet
 eventlet.monkey_patch()
 
 ###urls城市根据自己所处的地理位置修改
-urls = [
+#urls = [
     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9cWluaHVhbmdkYW8%3D",#秦皇岛
     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIGNpdHk9InRhbmdzaGFuIg%3D%3D",#唐山
     "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjQxMzQiICYmIHJlZ2lvbj0iaGViZWki",#河北
@@ -19,10 +19,13 @@ urls = [
     "https://site.ip138.com/mail.petzhu.top/"#河北
 ]
 
-for url in urls:
-    response = requests.get(url, timeout=0.5)
-    if response.status_code == 200:
-        print(url)
-    else:
-        continue
+url = 'https://example.com'
+try:
+    response = requests.get(url, timeout=5)  # 设置超时时间为5秒
+    response.raise_for_status()  # 检查是否请求成功
+    print(response.text)
+except requests.exceptions.Timeout:
+    print("请求超时，请检查网络或尝试增加超时时间。")
+except requests.exceptions.RequestException as e:
+    print(f"请求发生异常：{e}")
     
